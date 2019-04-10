db501b2f0218c082ef745d2510f65d78d2c40982
chen <543066440@qq.com>
2019-04-10 14:23:48 +0800

    tijiao

---
+++ public/src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * <p>Title: test</p>
+ * <p>Describe: TODO</p>
+ * @author wangsp
+ * @date 2019年4月10日
+ */
+public class test {
+	public static void main(String[] args) {
+		System.out.println("XX");
+	}
+}
---
+++ public/src/test2/test2.java
@@ -0,0 +1,14 @@
+package test2;
+
+/**
+ * <p>Title: test2</p>
+ * <p>Describe: TODO</p>
+ * @author wangsp
+ * @date 2019年4月10日
+ */
+public class test2 {
+	
+	public void set(){
+		System.out.println("GIT2");
+	}
+}
