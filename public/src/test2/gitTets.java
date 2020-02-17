f2577663dd4fd404c494edb034a252a045642e45
543066440 <49465111+543066440@users.noreply.github.com>
2020-02-17 20:03:42 +0800

    Update gitTets.java

--- public/src/test2/gitTets.java
+++ public/src/test2/gitTets.java
@@ -3,7 +3,6 @@ package test2;
 public class gitTets {
 	
 	public static void main(String[] args) {
-		System.out.println("1111");
-        System.out.println("2222");
+	System.out.println("1111");
 	}
 }
