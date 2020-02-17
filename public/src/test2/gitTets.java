dca251a5977cf62fdda4c6dd4bd710285cae6089
qq543 <qq543@DESKTOP-P92PFDL>
2020-02-17 19:48:08 +0800

    11

--- public/src/test2/gitTets.java
+++ public/src/test2/gitTets.java
@@ -3,8 +3,7 @@ package test2;
 public class gitTets {
 	
 	public static void main(String[] args) {
-		System.out.println("1111"); 
-
+		System.out.println("1111");
 		System.out.println();
 		System.out.println("222");
 		System.out.println(); 
