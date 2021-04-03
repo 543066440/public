ff2a25149cde0aba081f3d925b761f79102a3857
cwl <543066440@qq.com>
2021-04-03 19:42:15 +0800

    update .gitignore

---
+++ public/src/test/test1.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		String  account ="1,2,3,4,45,5,";
+		  account = account.substring(0, account.length() -1);
+		System.out.println(account);
+	}
+
+}
