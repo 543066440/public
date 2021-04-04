7728cdcf4f693d67e076399b16ffc6c3a88db7f3
cwl <543066440@qq.com>
2021-04-04 22:22:01 +0800

    确实

--- public/src/test/textclass.java
+++ public/src/test/textclass.java
@@ -3,9 +3,7 @@ package test;
 public class textclass {
 	
 	public static void main(String[] args) {
-		System.out.println("teddwwwwwwwddadst");
-		System.out.println("tedwdadxttwwwwedwdadst");
-		System.out.println("tedwdadxttwwwwedwdadst");
+
 	}
 
 }
